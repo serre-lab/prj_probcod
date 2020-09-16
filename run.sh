250412391afe34832fdd9afe2b9763094743443d
VictorBoutin <boutin.victor@gmail.com>
2020-09-16 10:52:05 +0200

    updated run.sh, added a classifier, and updated the training of the VAE

--- run.sh
+++ run.sh
@@ -1,5 +1,38 @@
 #!/bin/sh
 
+python3 train_classifier.py   --path ../simulation/ \
+                              --batch-size 64 \
+                              --log-interval 100 \
+                              --epochs 30
 
-python3 train_vae.py --lr_svi 1e-4 --nb_epoch 1 --lr 1e-3 --path ../simulation/ --type IVAE
+#python3 train_vae.py  --lr_svi 1e-4 \
+#                      --nb_epoch 200 \
+#                      --lr 1e-3 \
+#                      --path ../simulation/ \
+#                      --type IVAE \
+#                      --archi 512 256 40 \
+#                      --nb_it 20
 
+#python3 train_vae.py  --lr_svi 1e-4 \
+#                      --nb_epoch 200 \
+#                      --lr 1e-3 \
+#                      --path ../simulation/ \
+#                      --type IVAE \
+#                      --archi 512 256 20 \
+#                      --nb_it 20
+
+#python3 train_vae.py  --lr_svi 1e-4 \
+#                      --nb_epoch 200 \
+#                      --lr 1e-3 \
+#                      --path ../simulation/ \
+#                      --type IVAE \
+#                      --archi 512 256 10 \
+#                      --nb_it 20
+
+#python3 train_vae.py  --lr_svi 1e-4 \
+#                      --nb_epoch 200 \
+#                      --lr 1e-3 \
+#                      --path ../simulation/ \
+#                      --type IVAE \
+#                      --archi 512 256 5 \
+#                      --nb_it 20
--- train_vae.py
+++ train_vae.py
@@ -17,13 +17,18 @@ def main():
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate of the amortized inference')
     parser.add_argument('--path', type=str, default='', help='path to store the trained network')
     parser.add_argument('--type', type=str, default='IVAE', help='could be IVAE or VAE')
+    parser.add_argument('--archi', type=int, nargs='+', default=[512,256,20], help='achitecture of the encoder')
+    ## To do : Do not hardcode the 3 layer architecture in the VAE network
+    parser.add_argument('--device', type=str, default='cuda:7', help='gpu name')
+    parser.add_argument('--nb_it', type=int, default=20, help='number of iteration if the iterative inference')
+
     args = parser.parse_args()
 
-
-    device = 'cuda:7'
+    #device = 'cuda:7'
     batch_size = 1024
     kwargs = {'batch_size': batch_size,
               'drop_last': True,
+              'pin_memory': True,
               'num_workers': 8}
 
     grid_param = {'padding': 2, 'normalize': True,
@@ -40,17 +45,16 @@ def main():
     #test_loader = torch.utils.data.DataLoader(dataset2, **kwargs, shuffle=True)
 
     torch.manual_seed(1)
-    nb_epoch = 200
-    lr = 1e-3
-    z_dim = 10
+    #nb_epoch = 200
+    #lr = 1e-3
+    #z_dim = 10
     update_rate = 50
-    h_dim1 = 256
-    h_dim2 = 128
-    nb_iteration = 50
+    #h_dim1 = 256
+    #h_dim2 = 128
+    #nb_iteration = 50
     lr_SVI = args.lr_svi
     #lr_SVI = 1e-2
-    #hybrid = True
-    vae = VAE(x_dim = 28**2, z_dim=z_dim, h_dim1=h_dim1, h_dim2=h_dim2).to(device)
+    vae = VAE(x_dim = 28**2, h_dim1=args.archi[0], h_dim2=args.archi[1], z_dim=args.archi[-1]).to(args.device)
     mu_p = nn.Parameter()
     log_var_p = nn.Parameter()
     param_svi = [mu_p, log_var_p]
@@ -66,13 +70,6 @@ def main():
     all_param = vae.param_enc + vae.param_dec + param_svi
     disable_grad(all_param)
 
-    # for p in vae.parameters():
-    #    print(p.requires_grad)
-
-    # for p in param_svi:
-    #    print(p.requires_grad)
-
-
     # scheduler = StepLR(optimizer, step_size=update_rate, gamma=0.5)
     for idx_epoch in range(args.nb_epoch):
         train_reco_loss = 0
@@ -82,45 +79,43 @@ def main():
         vae.train()
 
         for batch_idx, (data, label) in enumerate(train_loader):
-            data, label = data.to(device), label.to(device)
+            data = data.to(args.device)
             data = torch.bernoulli(data)
+            ## Initialise the posterior output
             mu_p.data, log_var_p.data = vae.encoder(data.view(-1, 784))
-            enable_grad(param_svi)
 
-            for idx_it in range(nb_iteration):
+            ## Iterative refinement of the posterior
+            enable_grad(param_svi)
+            for idx_it in range(args.nb_it):
                 optimizer_SVI.zero_grad()
                 z = vae.sampling(mu_p, log_var_p)
                 reco = vae.decoder(z)
                 loss_gen, reco_loss, KL_loss = loss_function(reco, data, mu_p, log_var_p)
                 loss_gen.backward()
                 optimizer_SVI.step()
-
             disable_grad(param_svi)
 
+            ## Amortized learning of the likelihood parameter
             enable_grad(vae.param_dec)
-
             optimizer.zero_grad()
             z = vae.sampling(mu_p, log_var_p)
             reco = vae.decoder(z)
             loss_gen, reco_loss, KL_loss = loss_function(reco, data, mu_p, log_var_p)
-
             loss_gen.backward()
             optimizer.step()
-
             disable_grad(vae.param_dec)
 
+            ## Amortized learning of the posterior parameter
             enable_grad(vae.param_enc)
             optimizer.zero_grad()
-
             mu, log_var = vae.encoder(data.view(-1, 784))
             z = vae.sampling(mu, log_var)
             reco = vae.decoder(z)
             loss_gen, reco_loss, KL_loss = loss_function(reco, data, mu, log_var)
-
             loss_gen.backward()
             optimizer.step()
-
             disable_grad(vae.param_enc)
+
 
             train_reco_loss += reco_loss
             train_KL_loss += KL_loss
@@ -130,7 +125,7 @@ def main():
             print("original image")
             img_to_plot = make_grid(data[0:8, :, :, :], **grid_param)
             show(img_to_plot.detach().cpu())
-            print("recontruction image")
+            print("recontructed image")
             x_reco = reco.view(len(data), 1, 28, 28)
             img_to_plot = make_grid(x_reco[0:8, :, :, :], **grid_param)
             show(img_to_plot.detach().cpu())
@@ -151,7 +146,8 @@ def main():
             to_save =  {'model' : vae.state_dict(),
                         'type' : args.type}
 
-            torch.save(to_save, args.path + '{}_lrsvi={}_lr={}.pth'.format(args.type, args.lr_svi, args.lr))
+            torch.save(to_save, args.path + '{}_lrsvi={}_lr={}_enc={}_it={}.pth'.format(args.type, args.lr_svi, args.lr,
+                                                                                        str(args.archi), args.nb_it))
 
 
 if __name__ == '__main__':
