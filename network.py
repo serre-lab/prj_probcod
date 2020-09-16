3481db426198ca99414f7669d15543eaa9dbaa22
aimzer <aimen.zerroug@gmail.com>
2020-09-16 17:52:08 +0200

    classifier

--- network.py
+++ network.py
@@ -2,6 +2,31 @@ import torch.nn as nn
 import torch
 import torch.nn.functional as F
 
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+        
 class VAE(nn.Module):
     def __init__(self, x_dim, z_dim, h_dim1=512, h_dim2=256):
         super(VAE, self).__init__()
