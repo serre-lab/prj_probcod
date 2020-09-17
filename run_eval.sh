14aaa2cfd50f7672e218180cc361833e45513902
VictorBoutin <boutin.victor@gmail.com>
2020-09-17 09:26:34 +0200

    added automatic device selection in the run_eval

--- run_eval.sh
+++ run_eval.sh
@@ -5,8 +5,11 @@ device=7
 batch_size=1000
 
 config="config_eval.json"
-PathVAE='../simulation/IVAE_lrsvi=0.0001_lr=0.001_enc=[512,256,10]_it=20.pth'
-PathClassifier="../simulation/CL.pth"
+#PathVAE='../simulation/IVAE_lrsvi=0.0001_lr=0.001_enc=[512,256,10]_it=20.pth'
+PathVAE='../prj_probcode_exps/test_IVAE_lrsvi=1e-4_lr=1e-3_nb_it=20_[512,256,5]/model_results.pth'
+
+PathClassifier='../prj_probcode_exps/test_CL/model_results.pth'
+#PathClassifier="../simulation/CL.pth"
 
 
 CUDA_VISIBLE_DEVICES=$device python3 eval.py  \
